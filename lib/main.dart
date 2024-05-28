e733b13540824028d5a649951512603431407df0
=elangyudha280 <=elangyudharakasiwi2@gmail.com>
2024-05-28 12:27:07 -0700

    Setup project

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hub/widget/home/homeWidget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  ConfigComponent createState() => ConfigComponent();
+}
+
+// widget config component
+class ConfigComponent extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fruit Hub',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/':Home()
+      },
+    )
+  }
+}
---
+++ lib/widget/home/homeWidget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hub/main.dart';
+
+// COMPONENT HOME
+class Home extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+    );
+  }
+}
